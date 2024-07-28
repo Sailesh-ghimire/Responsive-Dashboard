bba4d61c79d6c9ed065b7aabd56062912fe9669d
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2024-07-28 12:01:12 +0545

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate,
